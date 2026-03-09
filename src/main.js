35cac2f59409aead185ce2cc82352615a5170c45
Julio Anccasi <julio.anccasi.dev@gmail.com>
2026-03-09 15:12:35 -0500

    Refactor code structure for improved readability and maintainability

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
-console.log('object');
+const ratingForm = document.getElementById('ratingForm');
+const ratingOptions = document.getElementById('ratingOptions');
+const ratingOptionButtons = ratingOptions.querySelectorAll('button');
+const submitBtn = document.getElementById('submitBtn');
+const selectedRating = document.getElementById('selectedRating');
+const messageCard = document.getElementById('messageCard');
+
+ratingForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (selectedRating.textContent === '0') return;
+
+  setTimeout(() => {
+    ratingForm.classList.add('rating-card__disabled');
+
+    messageCard.classList.add('message-card--enabled');
+  }, 1000);
+});
+
+ratingOptionButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    ratingOptionButtons.forEach((btn) => {
+      btn.classList.remove('rating-card__option--active');
+    });
+
+    button.classList.toggle('rating-card__option--active');
+
+    selectedRating.textContent = button.textContent;
+  });
+});
